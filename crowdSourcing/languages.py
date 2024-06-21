8abbf893c41e9eb8261d5d0292ce09cd987162ca
Zoe Stephens <zoestephens@Zoes-MacBook-Air.local>
2024-06-21 14:50:45 -0400

    stacked bar chart

--- crowdSourcing/languages.py
+++ crowdSourcing/languages.py
@@ -2,15 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 data = pd.read_csv("crowdSourcing/allData.csv",index_col=False)
 percentData = pd.read_csv("crowdSourcing/percentData.csv")
-for row in percentData:
-    print(row)
 #plt.pie(data['What language do you speak to your Wellesley friends in?'],labels=data['LANGUAGE'])
 #plt.show()
 
-'''percentEnglish = []
+
+
+percentEnglish = []
 for col in data.drop("LANGUAGE",axis=1).columns:
     percentEnglish.append(data[col].iloc[0]/data[col].iloc[-1])
 #print(percentEnglish)
+
 
 categories = data.drop("LANGUAGE",axis=1).columns
 fig, ax = plt.subplots(figsize=(8, 4))
@@ -21,6 +22,8 @@ ax.set_xticklabels(categories, rotation=30, ha='right')
 plt.subplots_adjust(bottom=0.5)
 plt.show()
 
-
 percentData.plot(x='LANGUAGE',kind="bar",stacked=True)
-plt.show()'''
+
+# Put a legend to the right of the current axis
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
